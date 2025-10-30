efca77bcd5a4e43a6419c8f3790215504bc60438
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-10-30 16:37:59 +0900

    feat: レイアウトとホームページのコンテンツを更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import SearchGarbage from "../components/payments/page";
+
 export default function Home() {
   return (
-    <div className="flex flex-1 items-center justify-center">
-      <p>main page</p>
-    </div>
+    <main className="flex flex-1 items-start justify-center bg-background-main pt-30">
+      <div className="container max-w-3xl px-4">
+        <div className="text-center space-y-3">
+          <h1 className="text-4xl md:text-5xl font-bold tracking-tight leading-tight">
+            捨てる神あれば{" "}
+            <span className="text-muted-foreground">拾う神あり。</span>
+          </h1>
+          <p className="text-base text-muted-foreground">
+            捨てたいごみを入力してください。
+          </p>
+        </div>
+
+        <SearchGarbage />
+      </div>
+    </main>
   );
 }
