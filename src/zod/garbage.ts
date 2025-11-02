c461eda8ede3ad4b3be13e153faaa9b3f087a1a1
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-11-02 19:41:40 +0900

    feat: ごみ分別フォームスキーマを追加

---
+++ src/zod/garbage.ts
@@ -0,0 +1,17 @@
+import { garbageCategories, garbageItems } from "@/db/schemas/garbages"
+import { createInsertSchema } from "drizzle-zod"
+import { z } from "zod"
+
+export const garbageCategoryFormSchema = createInsertSchema(garbageCategories, {
+  name: z.string().trim().min(1, "分別区分名称は1文字以上入力してください")
+}).omit({
+  id: true,
+});
+
+export const garbageItemFormSchema = createInsertSchema(garbageItems, {
+  name: z.string().trim().min(1, "分別区分名称は1文字以上入力してください"),
+  garbageCategory: z.number().int().positive("分別区分を選択してください"),
+  note: z.string().optional(),
+}).omit({
+  id: true,
+});
