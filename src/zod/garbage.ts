bbe4e42cdcf51c8aee5ada847674302b21e38a53
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-11-08 15:25:08 +0900

    fix: garbageItemFormSchemaのエラーメッセージを修正

--- src/zod/garbage.ts
+++ src/zod/garbage.ts
@@ -9,7 +9,7 @@ export const garbageCategoryFormSchema = createInsertSchema(garbageCategories, {
 });
 
 export const garbageItemFormSchema = createInsertSchema(garbageItems, {
-  name: z.string().trim().min(1, "分別区分名称は1文字以上入力してください"),
+  name: z.string().trim().min(1, "品目名は1文字以上入力してください"),
   garbageCategory: z.number().int().positive("分別区分を選択してください"),
   note: z.string().optional(),
 }).omit({
