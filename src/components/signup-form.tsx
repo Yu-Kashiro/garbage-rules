a0578f7239b71940755c3c2a570d8affad34c0d3
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-11-10 10:10:44 +0900

    feat: サインアップフォームコンポーネントを追加し、ログインフォームのリンクを修正

--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -15,7 +15,7 @@ import {
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
 
-export function LoginForm({
+export function SignupForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
@@ -23,9 +23,9 @@ export function LoginForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader>
-          <CardTitle>Login to your account</CardTitle>
+          <CardTitle>Create an account</CardTitle>
           <CardDescription>
-            Enter your email below to login to your account
+            Enter your email below to create your account
           </CardDescription>
         </CardHeader>
         <CardContent>
@@ -41,24 +41,20 @@ export function LoginForm({
                 />
               </Field>
               <Field>
-                <div className="flex items-center">
-                  <FieldLabel htmlFor="password">Password</FieldLabel>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
+                <FieldLabel htmlFor="password">Password</FieldLabel>
                 <Input id="password" type="password" required />
               </Field>
               <Field>
-                <Button type="submit">Login</Button>
+                <FieldLabel htmlFor="confirm-password">Confirm Password</FieldLabel>
+                <Input id="confirm-password" type="password" required />
+              </Field>
+              <Field>
+                <Button type="submit">Sign up</Button>
                 <Button variant="outline" type="button">
-                  Login with Google
+                  Sign up with Google
                 </Button>
                 <FieldDescription className="text-center">
-                  Don&apos;t have an account? <a href="/signup">Sign up</a>
+                  Already have an account? <a href="/login">Login</a>
                 </FieldDescription>
               </Field>
             </FieldGroup>
