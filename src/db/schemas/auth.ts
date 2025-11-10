5d958fa53264553d52daedb498bf66caaabe60ca
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-11-10 13:43:48 +0900

    feat: ユーザーテーブルに匿名ユーザーを示すカラムを追加し、スキーマを更新

--- src/db/schemas/auth.ts
+++ src/db/schemas/auth.ts
@@ -10,6 +10,7 @@ export const users = sqliteTable("users", {
     .default(false)
     .notNull(),
   image: text("image"),
+  isAnonymous: integer("is_anonymous", { mode: "boolean" }).default(false),
   ...timestamps,
 });
 
