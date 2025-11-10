2f6a366d860d481a041d0625b0885a24468e83f8
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-11-10 10:51:25 +0900

    feat: ログインフォームのテキストを日本語に翻訳し、スタイルを調整

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,19 +1,13 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Field,
   FieldDescription,
   FieldGroup,
   FieldLabel,
-} from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
 
 export function LoginForm({
   className,
@@ -22,17 +16,14 @@ export function LoginForm({
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
-        <CardHeader>
-          <CardTitle>Login to your account</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
+        <CardHeader className="justify-center">
+          <CardTitle>ログイン</CardTitle>
         </CardHeader>
         <CardContent>
           <form>
             <FieldGroup>
               <Field>
-                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <FieldLabel htmlFor="email">メールアドレス</FieldLabel>
                 <Input
                   id="email"
                   type="email"
@@ -41,24 +32,24 @@ export function LoginForm({
                 />
               </Field>
               <Field>
-                <div className="flex items-center">
-                  <FieldLabel htmlFor="password">Password</FieldLabel>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
+                <FieldLabel htmlFor="password">パスワード</FieldLabel>
                 <Input id="password" type="password" required />
               </Field>
               <Field>
-                <Button type="submit">Login</Button>
+                <Button type="submit">ログイン</Button>
                 <Button variant="outline" type="button">
-                  Login with Google
+                  Googleでログイン
                 </Button>
                 <FieldDescription className="text-center">
-                  Don&apos;t have an account? <a href="/signup">Sign up</a>
+                  <a href="/signup">新規登録はこちら</a>
+                </FieldDescription>
+                <FieldDescription className="text-center">
+                  <a
+                    href="#"
+                    className="inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    パスワードをお忘れですか？
+                  </a>
                 </FieldDescription>
               </Field>
             </FieldGroup>
@@ -66,5 +57,5 @@ export function LoginForm({
         </CardContent>
       </Card>
     </div>
-  )
+  );
 }
