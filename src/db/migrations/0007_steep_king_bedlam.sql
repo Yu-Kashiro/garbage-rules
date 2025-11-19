6c7eeaca89920f3d93f60d95a8f7c02d761277a0
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-11-19 10:04:29 +0900

    feat: cache_metadataテーブルの作成とスキーマ定義を追加

---
+++ src/db/migrations/0007_steep_king_bedlam.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `cache_metadata` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`version` integer DEFAULT 1 NOT NULL,
+	`created_at` integer DEFAULT (cast(unixepoch('subsecond') * 1000 as integer)) NOT NULL,
+	`updated_at` integer DEFAULT (cast(unixepoch('subsecond') * 1000 as integer)) NOT NULL
+);
