94f73dff2996fb53b195b39a24fb61d8e0dbc275
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-11-21 23:47:30 +0900

    style: ガーベジルールページのレイアウトを調整し、余白とパディングを最適化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import { Suspense } from "react";
 
 export default function GarbageRulesPage() {
   return (
-    <main className="flex flex-1 items-start justify-center bg-background-main pt-10 md:pt-10">
-      <div className="container max-w-3xl px-4 space-y-10">
+    <main className="flex flex-1 items-start justify-center bg-background-main pt-4 md:pt-10">
+      <div className="container max-w-3xl px-2 md:px-4 space-y-5 md:space-y-10">
         <h1 className="text-center text-xl font-bold md:text-3xl">
           捨てたいごみを入力してください。
         </h1>
 
-        <div className="sticky top-14 z-10 bg-background-main pb-6 pt-2">
+        <div className="sticky top-14 z-10 bg-background-main md:pb-6 pt-2">
           <Suspense>
             <SearchForm />
           </Suspense>
