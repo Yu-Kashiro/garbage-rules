edc9531df6cc75be239068c7a686f9095b8074f8
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-11-30 15:46:45 +0900

    fix: 不要なantialiasedクラスを削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="ja" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} flex flex-col min-h-dvh antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} flex flex-col min-h-dvh`}
       >
         <NuqsAdapter>
           <ThemeProvider
